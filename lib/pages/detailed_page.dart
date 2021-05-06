4343cf9529ddd64fbaa87c3b4455a3ef24fd6758
kozgot <gogota97@gmail.com>
2021-05-06 21:22:08 +0200

    fix detailed page on web error

--- lib/pages/detailed_page.dart
+++ lib/pages/detailed_page.dart
@@ -13,48 +13,53 @@ class DetailedPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(photo.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          AspectRatio(
-            aspectRatio: 600 / 400,
-            child: Hero(
-              tag: photo.id,
-              child: Image.network(
-                photo.url,
-                loadingBuilder: (context, child, loading) {
-                  if (loading == null) {
-                    return child;
-                  } else {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                },
+
+      body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AspectRatio(
+                aspectRatio: 600 / 400,
+                child: Hero(
+                  tag: photo.id,
+                    child:Image.network(
+                    photo.url,
+                    loadingBuilder: (context, child, loading) {
+                      if (loading == null) {
+                        return child;
+                      } else {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    },
+                  ),
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  photo.title,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  photo.description,
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              )
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              photo.title,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              photo.description,
-              style: TextStyle(
-                fontSize: 14,
-              ),
-            ),
-          )
-        ],
-      ),
+        ),
     );
   }
 }
--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -15,7 +15,7 @@ class _ListPageState extends State<ListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Lists & Grids"),
+        title: Text("Image List"),
       ),
       body: SingleChildScrollView(
         child: Center(
@@ -35,139 +35,6 @@ class _ListPageState extends State<ListPage> {
           ),
         ),
       ),
-      /*body: ListView(
-        children: [
-          ...Iterable.generate(100000, (id) {
-            var item = StockPhoto.fromId(id);
-            return ListItemWidget(
-              item: item,
-            );
-          })
-        ],
-      ),*/
-      /*body: ListView.builder(
-        itemExtent: 80,
-        itemBuilder: (context, index){
-          var item = StockPhoto.fromId(index);
-          return ListItemWidget(item: item);
-        },
-      )*/
-      /*body: ListView.separated(
-        itemCount: 1000000,
-        itemBuilder: (context, index) {
-          var item = StockPhoto.fromId(index);
-          return ListItemWidget(item: item);
-        },
-        separatorBuilder: (context, index) => Container(
-          height: 16,
-          alignment: Alignment.center,
-          child: Container(
-            height: 1,
-            color: Colors.grey.withOpacity(1),
-          ),
-        ),
-      ),*/
-      /*body: GridView.count(
-        crossAxisCount: 3,
-        childAspectRatio: 1 / 1,
-        crossAxisSpacing: 4,
-        mainAxisSpacing: 4,
-        children: [
-          ...Iterable.generate(
-            100000,
-            (id) {
-              var item = StockPhoto.fromId(id);
-              return GridItemWidget(
-                item: item,
-              );
-            },
-          )
-        ],
-      ),*/
-      /*body: GridView.extent(
-        maxCrossAxisExtent: 100,
-        mainAxisSpacing: 4,
-        crossAxisSpacing: 4,
-        childAspectRatio: 1 / 1,
-        children: [
-          ...Iterable.generate(
-            100000,
-                (id) {
-              var item = StockPhoto.fromId(id);
-              return GridItemWidget(
-                item: item,
-              );
-            },
-          )
-        ],
-      )*/
-      /*body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          mainAxisExtent: 100,
-          crossAxisCount: 4,
-          mainAxisSpacing: 4,
-          crossAxisSpacing: 4,
-          childAspectRatio: 1 / 1,
-        ),
-        itemBuilder: (context, index) {
-          var item = StockPhoto.fromId(index);
-          return GridItemWidget(item: item);
-        },
-      ),*/
-      /*body: Scrollbar(
-        child: RefreshIndicator(
-          onRefresh: () async {
-            setState(() {
-              startingIndex = Random().nextInt(200);
-            });
-          },
-          child: CustomScrollView(
-            physics: AlwaysScrollableScrollPhysics(),
-            slivers: [
-              SliverAppBar(
-                title: Text("Flexible list view"),
-                expandedHeight: 240,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Image.network(
-                    "https://picsum.photos/600/400",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                floating: true,
-                snap: true,
-                pinned: true,
-              ),
-              SliverPadding(
-                padding: EdgeInsets.all(16),
-                sliver: SliverList(
-                  delegate: SliverChildListDelegate([
-                    ...Iterable.generate(
-                      listOffset,
-                      (id) {
-                        var item = StockPhoto.fromId(id + startingIndex);
-                        return ListItemWidget(
-                          item: item,
-                        );
-                      },
-                    )
-                  ]),
-                ),
-              ),
-              SliverGrid(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    var item = StockPhoto.fromId(index + startingIndex + listOffset);
-                    return GridItemWidget(item: item);
-                  },
-                  childCount: 10,
-                ),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, crossAxisSpacing: 4, mainAxisSpacing: 4),
-              )
-            ],
-          ),
-        ),
-      ),*/
     );
   }
 }
