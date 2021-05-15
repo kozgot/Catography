6e3b2955a0ff962c0d0613792ac93748de6834e0
kozgot <gogota97@gmail.com>
2021-05-15 13:34:53 +0200

    Display additional breed info

--- lib/ui/details/detailed_page.dart
+++ lib/ui/details/detailed_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:catography/domain/model/cat_image.dart';
 import 'package:flutter/material.dart';
@@ -36,12 +38,137 @@ class ImageDetails extends StatelessWidget {
               ),
                 Padding(
                   padding: const EdgeInsets.only(top: 8.0),
-                  child: Text(catImage.id),
+                  child: Text(catImage.breed != null ? catImage.breed!.name : "No breed",
+                    style: TextStyle(fontWeight: FontWeight.normal, fontSize: 20),),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+                  child: Text(catImage.breed != null && catImage.breed!.description != null ? catImage.breed!.description! : "No breed",
+                      style: TextStyle(fontWeight: FontWeight.normal, fontSize: 16),
+                    textAlign: TextAlign.justify,
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 8.0),
-                  child: Text(catImage.breed != null ? catImage.breed!.name : "No breed"),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.workspaces_filled,
+                        size: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4.0),
+                        child: Text('Temperament: '),
+                      ),
+                    ],
+                  ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Wrap( children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+                          child: Text(
+                            catImage.breed != null ? catImage.breed!.temperament! : "Unknown",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                  ])
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.child_friendly,
+                        size: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4.0),
+                        child: Text('Child friendly: '),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4.0),
+                        child: Text(
+                          catImage.breed != null && catImage.breed!.childFriendly != null
+                              ? catImage.breed!.childFriendly! > 0 ? "Yes" : "No"
+                              : "Unknown",
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.add_circle,
+                        size: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4.0),
+                        child: Text('Dog friendly: '),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4.0),
+                        child: Text(
+                          catImage.breed != null && catImage.breed!.dogFriendly != null
+                              ? catImage.breed!.dogFriendly! > 0 ? "Yes" : "No"
+                              : "Unknown",
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.watch_later,
+                        size: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4.0),
+                        child: Text('Lifespan: '),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4.0),
+                        child: Text(
+                          catImage.breed != null && catImage.breed!.lifeSpan != null ? catImage.breed!.lifeSpan! : "Unknown",
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        size: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4.0),
+                        child: Text('Origin: '),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4.0),
+                        child: Text(
+                          catImage.breed != null ? catImage.breed!.origin : "Unknown",
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
                 Padding(
                   padding: const EdgeInsets.only(top: 8.0),
                     child: Row(
